8789285d0e4645b23efe1e4938acddc4921b7195
Violette Jacobs <72454626+VioletteJacobs@users.noreply.github.com>
2021-02-14 18:42:41 +0100

    hook loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
+import React, {useState, useEffect} from "react";
 import './App.css';
+import Loader from "./Loader"
 
 function App() {
-  return (
-    <div className="App">
+  // use effect démarrant à true car de bas on veut voir le gif. 
+  const [loader, setLoader] = useState(true)
 
+  // quand le composant à fini de se charger tableau vide pour qu'il se lance seulement la première fois: timeOut rendant le gif invisible au bout d e3secondes.
+
+  useEffect(() => {
+    setTimeout(()=>{
+      setLoader(false);
+    },3000)
+
+  }, [])
+  // return conditionnel si loader est true alors on envoie le gif, sinon on envoit le site. 
+  return loader? (
+    <Loader />
+  ): (
+    <div className="App">
+      <h1>Test de loading</h1>
+      
     </div>
   );
 }
--- src/Loader.js
+++ src/Loader.js
@@ -4,7 +4,7 @@ import LoaderGif from "./ping.gif"
 export default function Loader() {
     return (
         <div>
-            
+            <img src={LoaderGif} alt="gif raquette ping pong"/>
         </div>
     )
 }
